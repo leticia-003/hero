1a539d88a6ece2a6a67fba67139ad8e72678245f
leticia-003 <leticiamendes2122@gmail.com>
2022-10-10 14:46:47 +0100

    imported lanterna

---
+++ src/main/java/Game.java
@@ -0,0 +1,34 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+    public Game () {
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        }
+        catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        private void draw () {
+
+        }
+        public void run () {
+            
+        }
+    }
+}
